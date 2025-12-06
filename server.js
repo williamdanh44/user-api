2d62ca0204cce1d385242757a6b3bae257a3a50c
William Danh <williamto.992@gmail.com>
2025-12-05 21:05:37 -0500

    good now

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -9,7 +8,7 @@ const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const passportJWT = require('passport-jwt');
 
-const HTTP_PORT = process.env.PORT || 8080;
+const app = express();
 
 // ------------------ PASSPORT JWT SETUP ------------------
 let ExtractJwt = passportJWT.ExtractJwt;
@@ -33,6 +32,7 @@ let strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
 
 passport.use(strategy);
 
+// ------------------ MIDDLEWARE ------------------
 app.use(express.json());
 app.use(cors());
 app.use(passport.initialize());
@@ -40,68 +40,78 @@ app.use(passport.initialize());
 // ------------------ ROUTES ------------------
 
 // Register a new user
-app.post("/api/user/register", (req, res) => {
-    userService.registerUser(req.body)
-        .then(msg => res.json({ message: msg }))
-        .catch(msg => res.status(422).json({ message: msg }));
+app.post("/api/user/register", async (req, res) => {
+    try {
+        const msg = await userService.registerUser(req.body);
+        res.status(201).json({ message: msg });
+    } catch (err) {
+        res.status(422).json({ message: err });
+    }
 });
 
 // Login and return JWT token
-app.post("/api/user/login", (req, res) => {
-    userService.checkUser(req.body)
-        .then(user => {
-            // Create JWT payload
-            let payload = {
-                _id: user._id,
-                userName: user.userName
-            };
-            // Sign token with secret from .env
-            let token = jwt.sign(payload, process.env.JWT_SECRET);
-            res.json({ message: "login successful", token: token });
-        })
-        .catch(msg => res.status(422).json({ message: msg }));
+app.post("/api/user/login", async (req, res) => {
+    try {
+        const user = await userService.checkUser(req.body);
+        const payload = {
+            _id: user._id,
+            userName: user.userName
+        };
+        const token = jwt.sign(payload, process.env.JWT_SECRET);
+        res.json({ message: "login successful", token });
+    } catch (err) {
+        res.status(422).json({ message: err });
+    }
 });
 
 // Protected: Get user favourites
 app.get("/api/user/favourites",
     passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-        userService.getFavourites(req.user._id)
-            .then(data => res.json(data))
-            .catch(msg => res.status(422).json({ error: msg }));
+    async (req, res) => {
+        try {
+            const data = await userService.getFavourites(req.user._id);
+            res.json(data);
+        } catch (err) {
+            res.status(422).json({ error: err });
+        }
     }
 );
 
 // Protected: Add favourite
 app.put("/api/user/favourites/:id",
     passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-        userService.addFavourite(req.user._id, req.params.id)
-            .then(data => res.json(data))
-            .catch(msg => res.status(422).json({ error: msg }));
+    async (req, res) => {
+        try {
+            const data = await userService.addFavourite(req.user._id, req.params.id);
+            res.json(data);
+        } catch (err) {
+            res.status(422).json({ error: err });
+        }
     }
 );
 
 // Protected: Remove favourite
 app.delete("/api/user/favourites/:id",
     passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-        userService.removeFavourite(req.user._id, req.params.id)
-            .then(data => res.json(data))
-            .catch(msg => res.status(422).json({ error: msg }));
+    async (req, res) => {
+        try {
+            const data = await userService.removeFavourite(req.user._id, req.params.id);
+            res.json(data);
+        } catch (err) {
+            res.status(422).json({ error: err });
+        }
     }
 );
 
-// ------------------ SERVER START ------------------
+// ------------------ EXPORT APP FOR VERCEL ------------------
+// Vercel handles serverless function execution; DO NOT use app.listen()
 userService.connect()
     .then(() => {
-        app.listen(HTTP_PORT, () => {
-            console.log("API listening on: " + HTTP_PORT);
-        });
+        console.log("Database connected successfully.");
     })
     .catch(err => {
-        console.log("Unable to start server: " + err);
+        console.log("Unable to connect to database: " + err);
         process.exit(1);
     });
 
-module.exports = app; // Export for Vercel
+module.exports = app;
