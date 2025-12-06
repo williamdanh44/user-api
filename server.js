8c62214b945d68bee47f69bab2e875a6ec3be708
William Danh <williamto.992@gmail.com>
2025-12-05 21:38:10 -0500

    good now

--- server.js
+++ server.js
@@ -1,117 +1,96 @@
 const express = require('express');
+const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 const userService = require("./user-service.js");
 
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
-const passportJWT = require('passport-jwt');
+const HTTP_PORT = process.env.PORT || 8080;
 
-const app = express();
-
-// ------------------ PASSPORT JWT SETUP ------------------
-let ExtractJwt = passportJWT.ExtractJwt;
-let JwtStrategy = passportJWT.Strategy;
-
-let jwtOptions = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
-    secretOrKey: process.env.JWT_SECRET
-};
-
-let strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
-    if (jwt_payload) {
-        next(null, {
-            _id: jwt_payload._id,
-            userName: jwt_payload.userName
-        });
-    } else {
-        next(null, false);
-    }
-});
-
-passport.use(strategy);
-
-// ------------------ MIDDLEWARE ------------------
 app.use(express.json());
 app.use(cors());
-app.use(passport.initialize());
 
-// ------------------ ROUTES ------------------
-
-// Register a new user
-app.post("/api/user/register", async (req, res) => {
-    try {
-        const msg = await userService.registerUser(req.body);
-        res.status(201).json({ message: msg });
-    } catch (err) {
-        res.status(422).json({ message: err });
-    }
-});
-
-// Login and return JWT token
-app.post("/api/user/login", async (req, res) => {
-    try {
-        const user = await userService.checkUser(req.body);
-        const payload = {
-            _id: user._id,
-            userName: user.userName
-        };
-        const token = jwt.sign(payload, process.env.JWT_SECRET);
-        res.json({ message: "login successful", token });
-    } catch (err) {
-        res.status(422).json({ message: err });
-    }
-});
-
-// Protected: Get user favourites
-app.get("/api/user/favourites",
-    passport.authenticate('jwt', { session: false }),
-    async (req, res) => {
-        try {
-            const data = await userService.getFavourites(req.user._id);
-            res.json(data);
-        } catch (err) {
-            res.status(422).json({ error: err });
-        }
-    }
-);
-
-// Protected: Add favourite
-app.put("/api/user/favourites/:id",
-    passport.authenticate('jwt', { session: false }),
-    async (req, res) => {
-        try {
-            const data = await userService.addFavourite(req.user._id, req.params.id);
-            res.json(data);
-        } catch (err) {
-            res.status(422).json({ error: err });
-        }
-    }
-);
-
-// Protected: Remove favourite
-app.delete("/api/user/favourites/:id",
-    passport.authenticate('jwt', { session: false }),
-    async (req, res) => {
-        try {
-            const data = await userService.removeFavourite(req.user._id, req.params.id);
-            res.json(data);
-        } catch (err) {
-            res.status(422).json({ error: err });
-        }
-    }
-);
-
-// ------------------ EXPORT APP FOR VERCEL ------------------
-// Vercel handles serverless function execution; DO NOT use app.listen()
-userService.connect()
-    .then(() => {
-        console.log("Database connected successfully.");
-    })
-    .catch(err => {
-        console.log("Unable to connect to database: " + err);
-        process.exit(1);
+app.post("/api/user/register", (req, res) => {
+    userService.registerUser(req.body)
+    .then((msg) => {
+        res.json({ "message": msg });
+    }).catch((msg) => {
+        res.status(422).json({ "message": msg });
     });
+});
 
-module.exports = app;
+// Route for user login
+app.post("/api/user/login", (req, res) => {
+    userService.checkUser(req.body)  // Check user credentials
+        .then((user) => {
+            if (user) {
+                // Create JWT payload with user info
+                const payload = {
+                    _id: user._id,
+                    userName: user.userName
+                };
+
+                // Sign the payload to create a token
+                const token = jwt.sign(payload, process.env.JWT_SECRET, {
+                    expiresIn: '1h' // Token expires in 1 hour
+                });
+
+                // Send the signed token back to the client
+                res.json({
+                    message: "Login successful",
+                    token: token
+                });
+            } else {
+                res.status(422).json({ message: "Invalid username or password" });
+            }
+        })
+        .catch(msg => {
+            res.status(422).json({ message: msg });
+        });
+});
+
+// Protect /api/user/favourites route
+app.get("/api/user/favourites", passport.authenticate('jwt', { session: false }), (req, res) => {
+    userService.getFavourites(req.user._id)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(msg => {
+            res.status(422).json({ error: msg });
+        });
+});
+
+// Protect /api/user/favourites/:id route
+app.put("/api/user/favourites/:id", passport.authenticate('jwt', { session: false }), (req, res) => {
+    userService.addFavourite(req.user._id, req.params.id)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(msg => {
+            res.status(422).json({ error: msg });
+        });
+});
+
+// Protect /api/user/favourites/:id route for DELETE
+app.delete("/api/user/favourites/:id", passport.authenticate('jwt', { session: false }), (req, res) => {
+    userService.removeFavourite(req.user._id, req.params.id)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(msg => {
+            res.status(422).json({ error: msg });
+        });
+});
+
+
+userService.connect()
+.then(() => {
+    app.listen(HTTP_PORT, () => { console.log("API listening on: " + HTTP_PORT) });
+})
+.catch((err) => {
+    console.log("unable to start the server: " + err);
+    process.exit();
+});
+
+
+
