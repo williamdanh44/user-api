8ad134174c0b5de964305c8ca85706661b0fdfd4
William Danh <williamto.992@gmail.com>
2025-12-05 19:19:06 -0500

    Commit

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const app = express();
+const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -9,14 +8,11 @@ const jwt = require("jsonwebtoken");
 const passport = require("passport");
 const passportJWT = require("passport-jwt");
 
-console.log("MONGO_URL:", process.env.MONGO_URL);
-console.log("JWT_SECRET:", process.env.JWT_SECRET);
-
 let ExtractJwt = passportJWT.ExtractJwt;
 let JwtStrategy = passportJWT.Strategy;
 
 let jwtOptions = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("Bearer"),
   secretOrKey: process.env.JWT_SECRET
 };
 
@@ -33,83 +29,71 @@ let strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
 
 passport.use(strategy);
 
-const HTTP_PORT = process.env.PORT || 8080;
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(passport.initialize());
 
+// --- Routes ---
+
 app.post("/api/user/register", (req, res) => {
   userService.registerUser(req.body)
-    .then((msg) => {
-      res.json({ message: msg });
-    })
-    .catch((msg) => {
-      res.status(422).json({ message: msg });
-    });
+    .then(msg => res.json({ message: msg }))
+    .catch(msg => res.status(422).json({ message: msg }));
 });
 
 app.post("/api/user/login", (req, res) => {
   userService.checkUser(req.body)
-    .then((user) => {
-      let payload = {
-        _id: user._id,
-        userName: user.userName
-      };
-
+    .then(user => {
+      let payload = { _id: user._id, userName: user.userName };
       let token = jwt.sign(payload, process.env.JWT_SECRET);
-
-      res.json({ message: "login successful", token: token });
+      res.json({ message: "login successful", token });
     })
-    .catch((msg) => {
-      res.status(422).json({ message: msg });
-    });
+    .catch(msg => res.status(422).json({ message: msg }));
 });
 
 app.get("/api/user/favourites",
-  passport.authenticate('jwt', { session: false }),
+  passport.authenticate("jwt", { session: false }),
   (req, res) => {
     userService.getFavourites(req.user._id)
-      .then(data => {
-        res.json(data);
-      })
-      .catch(msg => {
-        res.status(422).json({ error: msg });
-      });
+      .then(data => res.json(data))
+      .catch(msg => res.status(422).json({ error: msg }));
   }
 );
 
 app.put("/api/user/favourites/:id",
-  passport.authenticate('jwt', { session: false }),
+  passport.authenticate("jwt", { session: false }),
   (req, res) => {
     userService.addFavourite(req.user._id, req.params.id)
-      .then(data => {
-        res.json(data);
-      })
-      .catch(msg => {
-        res.status(422).json({ error: msg });
-      });
+      .then(data => res.json(data))
+      .catch(msg => res.status(422).json({ error: msg }));
   }
 );
 
 app.delete("/api/user/favourites/:id",
-  passport.authenticate('jwt', { session: false }),
+  passport.authenticate("jwt", { session: false }),
   (req, res) => {
     userService.removeFavourite(req.user._id, req.params.id)
-      .then(data => {
-        res.json(data);
-      })
-      .catch(msg => {
-        res.status(422).json({ error: msg });
-      });
+      .then(data => res.json(data))
+      .catch(msg => res.status(422).json({ error: msg }));
   }
 );
 
-userService.connect()
-  .then(() => {
-    app.listen(HTTP_PORT, () => { console.log("API listening on: " + HTTP_PORT) });
-  })
-  .catch((err) => {
-    console.log("unable to start the server: " + err);
-    process.exit();
-  });
+// --- MongoDB Connection ---
+let dbConnectionPromise;
+
+function connectDB() {
+  if (!dbConnectionPromise) {
+    dbConnectionPromise = userService.connect()
+      .then(() => console.log("MongoDB connected"))
+      .catch(err => console.error("MongoDB connection error:", err));
+  }
+  return dbConnectionPromise;
+}
+
+// --- Export as serverless function ---
+module.exports = async (req, res) => {
+  await connectDB(); // ensure DB is connected
+  app(req, res);
+};
