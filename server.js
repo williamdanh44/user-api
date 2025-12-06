97bff5ce9a25897ccb04e14164f82d3f10ba34dc
William Danh <williamto.992@gmail.com>
2025-12-05 19:21:55 -0500

    Commit

--- server.js
+++ server.js
@@ -1,100 +1,66 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 const userService = require("./user-service.js");
 
-const jwt = require("jsonwebtoken");
-const passport = require("passport");
-const passportJWT = require("passport-jwt");
+const HTTP_PORT = process.env.PORT || 8080;
 
-console.log("MONGO_URL:", process.env.MONGO_URL);
-console.log("JWT_SECRET:", process.env.JWT_SECRET);
-
-// JWT setup
-let ExtractJwt = passportJWT.ExtractJwt;
-let JwtStrategy = passportJWT.Strategy;
-
-let jwtOptions = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
-  secretOrKey: process.env.JWT_SECRET
-};
-
-let strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
-  if (jwt_payload) {
-    next(null, {
-      _id: jwt_payload._id,
-      userName: jwt_payload.userName
-    });
-  } else {
-    next(null, false);
-  }
-});
-
-passport.use(strategy);
-
-// Middleware
 app.use(express.json());
 app.use(cors());
-app.use(passport.initialize());
 
-// Root route for browser check
-app.get("/", (req, res) => {
-  res.send("User API is running!");
-});
-
-// Registration
 app.post("/api/user/register", (req, res) => {
-  userService.registerUser(req.body)
-    .then((msg) => res.json({ message: msg }))
-    .catch((msg) => res.status(422).json({ message: msg }));
+    userService.registerUser(req.body)
+    .then((msg) => {
+        res.json({ "message": msg });
+    }).catch((msg) => {
+        res.status(422).json({ "message": msg });
+    });
 });
 
-// Login
 app.post("/api/user/login", (req, res) => {
-  userService.checkUser(req.body)
+    userService.checkUser(req.body)
     .then((user) => {
-      let payload = { _id: user._id, userName: user.userName };
-      let token = jwt.sign(payload, process.env.JWT_SECRET);
-      res.json({ message: "login successful", token });
-    })
-    .catch((msg) => res.status(422).json({ message: msg }));
+        res.json({ "message": "login successful"});
+    }).catch(msg => {
+        res.status(422).json({ "message": msg });
+    });
 });
 
-// Protected routes
-app.get("/api/user/favourites",
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
+app.get("/api/user/favourites", (req, res) => {
     userService.getFavourites(req.user._id)
-      .then(data => res.json(data))
-      .catch(msg => res.status(422).json({ error: msg }));
-  }
-);
+    .then(data => {
+        res.json(data);
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
 
-app.put("/api/user/favourites/:id",
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
+});
+
+app.put("/api/user/favourites/:id", (req, res) => {
     userService.addFavourite(req.user._id, req.params.id)
-      .then(data => res.json(data))
-      .catch(msg => res.status(422).json({ error: msg }));
-  }
-);
+    .then(data => {
+        res.json(data)
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+});
 
-app.delete("/api/user/favourites/:id",
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
+app.delete("/api/user/favourites/:id", (req, res) => {
     userService.removeFavourite(req.user._id, req.params.id)
-      .then(data => res.json(data))
-      .catch(msg => res.status(422).json({ error: msg }));
-  }
-);
+    .then(data => {
+        res.json(data)
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+});
 
-// Start server
 userService.connect()
-  .then(() => {
-    const PORT = process.env.PORT || 8080;
-    app.listen(PORT, () => console.log(`API listening on port ${PORT}`));
-  })
-  .catch(err => {
-    console.log("Unable to start the server: " + err);
-    process.exit(1);
-  });
+.then(() => {
+    app.listen(HTTP_PORT, () => { console.log("API listening on: " + HTTP_PORT) });
+})
+.catch((err) => {
+    console.log("unable to start the server: " + err);
+    process.exit();
+});
